23a966e6319be1180326f66f84a5603fba6b0b8b
DesignSensei <81391882+DesignSensei@users.noreply.github.com>
2024-05-13 09:03:32 +0100

    Add files via upload

---
+++ MadLibsGame.py
@@ -0,0 +1,10 @@
+name = input("Give an example of a name: ")
+verb = input("Write out three verbs ending in '-ing': ")
+action = input("What do you do in water? Ensure it ends in -s: ")
+adjective = input("List an adjective describing the person e.g smart, fair, bubbly: ")
+place = input("Name two places: ")
+food = input("Name three types of foods: ")
+subjects = input("List three academic subjects: ")
+
+story = f"{name} is a boy from Imo state, Nigeria, who loves {verb} and {action} whenever he visits his Uncle's house that has a pool.\nBeing a very {adjective} boy, he has interests in {subjects}, excelling in them greatly. His favourite things to eat are {food} and favourite places to visit are {place}"
+print(story)
